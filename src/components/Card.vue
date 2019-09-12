86dd84929c78854e2ffb400082aa9baa41eb83ef
Kit Jonathan Te <kjonathante@gmail.com>
2019-09-12 16:18:44 -0700

    Added a Card component for easier handling of remove and update.

---
+++ src/components/Card.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="card">
+    <div>
+      <slot />
+    </div>
+    <div>
+      <button v-on:click="$emit(`removeUser`)">x</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+.card {
+  border: 1px solid grey;
+  padding: 10px;
+  margin: 5px;
+  width: 150px;
+  height: 150px;
+}
+</style>
--- src/components/UsersPane.vue
+++ src/components/UsersPane.vue
@@ -1,9 +1,6 @@
 <template>
   <div>
-    <ApolloQuery
-      :query="require('../graphql/Users.gql')"
-      fetch-policy="cache-and-network"
-    >
+    <ApolloQuery :query="require('../graphql/Users.gql')" fetch-policy="cache-and-network">
       <template slot-scope="{ result: { data, loading, error } }">
         <p v-if="loading">Loading...</p>
 
@@ -12,13 +9,13 @@
         </div>
 
         <template v-if="data && data.users && data.users.length">
-          <p
-            v-for="user of data.users"
-            v-bind:key="user.id"
-            v-on:click="UserRemove(user.id)"
-          >
-            {{ user.name }}
-          </p>
+          <div class="flex-container">
+            <Card
+              v-for="user of data.users"
+              v-bind:key="user.id"
+              v-on:removeUser="UserRemove(user.id)"
+            >{{ user.name }}</Card>
+          </div>
         </template>
       </template>
     </ApolloQuery>
@@ -31,12 +28,17 @@
 </template>
 
 <script>
+import Card from './Card'
 import USER_ADD_MUTATION from '../graphql/UserAdd.gql'
 import USER_REMOVE_MUTATION from '../graphql/UserRemove.gql'
 
 import { cacheUserAdd, cacheUserRemove } from '../cache/users'
 
 export default {
+  components: {
+    Card
+  },
+
   data() {
     return {
       nameInput: ''
@@ -88,5 +90,8 @@ export default {
   }
 }
 </script>
-
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.flex-container {
+  display: flex;
+}
+</style>
