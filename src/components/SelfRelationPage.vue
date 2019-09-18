cff8fe266fe858ebdb69a419713072863802dcac
Kit Jonathan Te <kjonathante@gmail.com>
2019-09-17 21:38:56 -0700

    Created SelfRelationPage component. Modified UserItem to accept clientId as a prop.

---
+++ src/components/SelfRelationPage.vue
@@ -0,0 +1,16 @@
+<template>
+  <BasePage>
+    <SelfRelationPane />
+  </BasePage>
+</template>
+
+<script>
+import BasePage from './BasePage'
+import SelfRelationPane from './SelfRelationPane'
+export default {
+  components: {
+    BasePage,
+    SelfRelationPane
+  }
+}
+</script>
--- src/router.js
+++ src/router.js
@@ -14,13 +14,15 @@ export default new Router({
       component: Home
     },
     {
-      path: '/about',
-      name: 'about',
+      path: '/self-relation',
+      name: 'selfRelation',
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () =>
-        import(/* webpackChunkName: "about" */ './views/About.vue')
+        import(
+          /* webpackChunkName: "about" */ './components/SelfRelationPage.vue'
+        )
     }
   ]
 })
